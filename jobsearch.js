d9ff3eebcea830ffe59b3bb0fe91d07b2aef08fc
egrueter <egrueter@jobcase.com>
2016-01-24 20:20:47 -0500

    Add a rough fuzzy search feature

--- jobsearch.js
+++ jobsearch.js
@@ -3,9 +3,25 @@
 
 if (Meteor.isClient) {
   Meteor.subscribe('jobs');
+
+  // A rough fuzzy search..
+  Template.jobsearch.helpers({
+    'jobs': function(){
+     var regexp = new RegExp(Session.get('search/keyword'), 'i');
+     return Jobs.find({title: regexp}).fetch();
+    }
+  });
+
+  Template.jobsearch.events({
+    'keyup .jobsearch': function(event){
+      Session.set('search/keyword', event.target.value);
+      console.log(Session.get('search/keyword'));
+    }
+  });
 }
 
 if (Meteor.isServer) {
-  Meteor.methods({});
+  Meteor.methods({
+  });
 }
 
